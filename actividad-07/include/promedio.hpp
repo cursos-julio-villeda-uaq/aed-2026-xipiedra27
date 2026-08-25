9e68e8a6e4fa9cfc6d69fd271455aa284ac847d9
witchet43 <witchet43@gmail.com>
2026-08-24 18:50:28 -0600

    Agregar actividad-07/include/promedio.hpp

---
+++ actividad-07/include/promedio.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <vector>
+
+namespace aed {
+
+double calcularSuma(const std::vector<double>& calificaciones);
+double calcularPromedio(const std::vector<double>& calificaciones);
+
+}  // namespace aed
