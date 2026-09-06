e1078431f5fb44b114035fb2422a357edef742d9
nattPC <xipiedra27@gmail.com>
2026-09-06 14:44:00 -0600

    Prueba2

--- actividad-09/src/suma_regresiva.cpp
+++ actividad-09/src/suma_regresiva.cpp
@@ -1,14 +1,15 @@
 #include "suma_regresiva.hpp"
 
 long long sumaIterativa(unsigned int n) {
-    long long suma=0
-    for(unsigned int i=0; i>0; i++){
+    long long suma=0; 
+    for(unsigned int i=0; i>0; i--){
         suma = suma+i;
     }
     return 0;
 }
 
 long long sumaRecursiva(unsigned int n) {
-    // TODO: Implementa la suma regresiva usando recursividad y un caso base.
+    if(n==0)
     return 0;
+return n + sumaRecursiva;       
 }
