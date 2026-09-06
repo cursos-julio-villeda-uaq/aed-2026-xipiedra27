dd452f5e7e4338d584e1f53fd4de2729489a20b5
nattPC <xipiedra27@gmail.com>
2026-09-06 14:38:35 -0600

    Prueba1

--- actividad-09/src/suma_regresiva.cpp
+++ actividad-09/src/suma_regresiva.cpp
@@ -1,7 +1,10 @@
 #include "suma_regresiva.hpp"
 
 long long sumaIterativa(unsigned int n) {
-    // TODO: Implementa la suma regresiva usando un ciclo.
+    long long suma=0
+    for(unsigned int i=0; i>0; i++){
+        suma = suma+i;
+    }
     return 0;
 }
 
